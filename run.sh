93d5f262079a4b2aca507208bad55717f21f121d
jesusalc <jesusalc@gmail.com>
2017-02-17 20:39:36 +0100

    debug classes server side, added run for oops classes

--- run.sh
+++ run.sh
@@ -1,32 +1,73 @@
 #!/bin/bash
 #
-cd functional/
-echo ' '
-echo -e "\\033[38;5;93m === Terminal Test \\033[38;5;123m PHP 5 \\033[38;5;93m === \\033[38;0m ";
-echo ' '
-php index.php
-echo " "
-echo ' '
-echo -e "\\033[38;5;93m === Terminal Test \\033[38;5;123m PHP 7 \\033[38;5;93m === \\033[38;0m ";
-echo ' '
-php index.php7
-echo " "
-echo -e "\\033[38;5;93m === Server Test \\033[38;5;123m PHP 5 Serve \\033[38;5;93m === \\033[38;0m ";
-echo " "
-echo "http://localhost:9000/extra"
-echo "http://localhost:9000/template"
-echo " "
-echo " Control C to do next test"
-echo " "
-php -S localhost:9000 -f index.php
-wait
-echo " "
-echo -e "\\033[38;5;93m === Server Test \\033[38;5;123m PHP 7 Serve \\033[38;5;93m === \\033[38;0m ";
-echo " "
-echo "http://localhost:9090/extra"
-echo "http://localhost:9090/template"
-echo " "
-php -S localhost:9090 -f index.php7
-wait
-echo " "
-echo " "
+CURCWD=$(pwd)
+(
+  cd functional/
+  echo ' '
+  echo -e "\\033[38;5;93m === Terminal Test \\033[38;5;123m Funtional PHP 5 \\033[38;5;93m === \\033[38;0m ";
+  echo ' '
+  php index.php
+  echo " "
+  echo ' '
+  echo -e "\\033[38;5;93m === Terminal Test \\033[38;5;123m Funtional PHP 7 \\033[38;5;93m === \\033[38;0m ";
+  echo ' '
+  php index.php7
+  echo " "
+  echo -e "\\033[38;5;93m === Server Test \\033[38;5;123m Funtional PHP 5 Serve \\033[38;5;93m === \\033[38;0m ";
+  echo " "
+  echo "http://localhost:9000/extra"
+  echo "http://localhost:9000/template"
+  echo " "
+  echo " Control C to do next test"
+  echo " "
+  php -S localhost:9000 -f index.php
+  wait
+  echo " "
+  echo -e "\\033[38;5;93m === Server Test \\033[38;5;123m Funtional PHP 7 Serve \\033[38;5;93m === \\033[38;0m ";
+  echo " "
+  echo "http://localhost:9090/extra"
+  echo "http://localhost:9090/template"
+  echo " "
+  php -S localhost:9090 -f index.php7
+  wait
+  echo " "
+  echo " "
+)
+##cd $CURCWD;
+(
+  cd oop/classes5
+  echo ' '
+  echo -e "\\033[38;5;99m === Terminal Test \\033[38;5;125m OOP PHP 5 \\033[38;5;99m === \\033[38;0m ";
+  echo ' '
+  php index.php
+  echo " "
+  echo " "
+  echo -e "\\033[38;5;99m === Server Test \\033[38;5;125m OOP PHP 5 Serve \\033[38;5;99m === \\033[38;0m ";
+  echo " "
+  echo "http://localhost:9000/extra"
+  echo "http://localhost:9000/template"
+  echo " "
+  echo " Control C to do next test"
+  echo " "
+  php -S localhost:9000 -f index.php
+  wait
+
+)
+#cd $CURCWD;
+(
+  cd oop/classes7
+  echo ' '
+  echo -e "\\033[38;5;99m === Terminal Test \\033[38;5;125m OOP PHP 7 \\033[38;5;99m === \\033[38;0m ";
+  echo ' '
+  php index.php7
+  echo " "
+  echo -e "\\033[38;5;99m === Server Test \\033[38;5;125m OOP PHP 7 Serve \\033[38;5;99m === \\033[38;0m ";
+  echo " "
+  echo "http://localhost:9090/extra"
+  echo "http://localhost:9090/template"
+  echo " "
+  php -S localhost:9090 -f index.php7
+  wait
+  echo " "
+  echo " "
+)
